6fe716ac1947bdb577dd2ac7e3a40c3f368f2899
OlafPetersen <federicop526@gmail.com>
2023-11-20 19:38:24 -0300

    ultima modificacion

--- projecto.js
+++ projecto.js
@@ -4,11 +4,10 @@ class ProductManager {
         this.id = 0;
     }
 
-    getProducts() {
-        return this.products;
-    }
-
     addProduct(product) {
+        if (!product.title || !product.description || !product.price || !product.thumbnail || !product.code || !product.stock) {
+            throw new Error('Todos los campos son obligatorios');
+        }
         if (this.products.some(p => p.code === product.code)) {
             throw new Error('El código del producto ya existe');
         }
@@ -16,10 +15,15 @@ class ProductManager {
         this.products.push({ ...product, id: this.id });
     }
 
+    getProducts() {
+        return this.products;
+    }
+
     getProductById(id) {
         const product = this.products.find(p => p.id === id);
         if (!product) {
-            throw new Error('Producto no encontrado');
+            console.error('Not found');
+            return;
         }
         return product;
     }
@@ -28,14 +32,18 @@ class ProductManager {
 const pm = new ProductManager();
 console.log(pm.getProducts()); // []
 
-pm.addProduct({
-    title: 'producto prueba',
-    description: 'Este es un producto prueba',
-    price: 200,
-    thumbnail: 'Sin imagen',
-    code: 'abc123',
-    stock: 25
-});
+try {
+    pm.addProduct({
+        title: 'producto prueba',
+        description: 'Este es un producto prueba',
+        price: 200,
+        thumbnail: 'Sin imagen',
+        code: 'abc123',
+        stock: 25
+    });
+} catch (e) {
+    console.error(e.message); //Todos los campos son obligatorios
+}
 
 console.log(pm.getProducts()); // [{...}]
 
@@ -52,9 +60,5 @@ try {
     console.error(e.message); // El código del producto ya existe
 }
 
-try {
-    console.log(pm.getProductById(1)); // {...}
-    console.log(pm.getProductById(999)); // Error
-} catch (e) {
-    console.error(e.message); // Producto no encontrado
-}
+console.log(pm.getProductById(1)); // {...}
+console.log(pm.getProductById(999)); // Not found
